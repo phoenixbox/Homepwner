ddbe62192cc6b6e7d6396df05305e876a2babd5c
phoenixbox <shane@quickleft.com>
2013-11-20 22:57:29 -0700

    added the items view controller and the BNRitem classes

---
+++ ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
---
+++ ItemsViewController.m
@@ -0,0 +1,27 @@
+//
+//  ItemsViewController.m
+//  Homepwner
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+
+@implementation ItemsViewController
+
+- (id)init
+{
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    
+    if(self){
+    }
+    
+    return self;
+}
+
+-(id)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+@end
