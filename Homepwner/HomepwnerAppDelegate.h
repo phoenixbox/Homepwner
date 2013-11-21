aefbf392b6b23d95f91976f572fe12210599012a
phoenixbox <shane@quickleft.com>
2013-11-20 22:37:24 -0700

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Homepwner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Shane Rogers on 11/20/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HomepwnerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HomepwnerAppDelegate class]));
+    }
+}
